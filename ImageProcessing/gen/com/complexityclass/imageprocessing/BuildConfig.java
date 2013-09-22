fb1106f43737f0a57dc966afd07493de3309a929
Valeriy <complexityclass@gmail.com>
2013-09-22 22:54:49 +0400

    first commit

---
+++ ImageProcessing/gen/com/complexityclass/imageprocessing/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.complexityclass.imageprocessing;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
